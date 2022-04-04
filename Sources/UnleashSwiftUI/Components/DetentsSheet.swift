8a903f4d626abbcd446d948d86965f76a5839b3a
apemaia99 <alessandrodimaio99@gmail.com>
2022-04-04 21:27:21 +0200

    fixing detent sheet in landscape

--- Sources/UnleashSwiftUI/Components/DetentsSheet.swift
+++ Sources/UnleashSwiftUI/Components/DetentsSheet.swift
@@ -70,7 +70,7 @@ struct DetentsSheet<Content>: UIViewRepresentable where Content: View {
             if let edgeRadius = edgeRadius {
                 sheetController.preferredCornerRadius = (edgeRadius <= 50 ? edgeRadius : 50)
             }
-            sheetController.prefersEdgeAttachedInCompactHeight = true
+            sheetController.widthFollowsPreferredContentSizeWhenEdgeAttached = true
         }
         
         //Coordinator attach
